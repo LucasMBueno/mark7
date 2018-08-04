8e2d1c27a0f498c4d360d141d92782615e0ee0bf
Fernando Papito <eu@papito.io>
2018-08-04 12:39:12 -0300

    incluindo headless

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,6 +1,32 @@
 require 'capybara'
 require 'capybara/cucumber'
 
+@browser = ENV['BROWSER']
+
+if @browser.eql?('headless')
+  puts 'Executando com Headless'
+
+  Capybara.javascript_driver = :selenium
+  Capybara.run_server = false
+
+  args = ['--no-default-browser-check']
+
+  caps = Selenium::WebDriver::Remote::Capabilities.chrome(
+    'chromeOptions' => { 'args' => args }
+  )
+
+  Capybara.register_driver :selenium do |app|
+    Capybara::Selenium::Driver.new(
+      app,
+      browser: :remote,
+      url: 'http://selenium:4444/wd/hub',
+      desired_capabilities: caps
+    )
+  end
+else
+  puts 'Executando sem Headless'
+end
+
 Capybara.configure do |config|
   config.default_driver = :selenium
   config.app_host = 'https://mark7.herokuapp.com'
